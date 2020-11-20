e18998d83a022c814bf933e5d063bf08de13884d
张伟 <zhangwei@zhangweideMacBook-Air.local>
2020-11-20 10:01:39 +0800

    init zwei version

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:gbk -*-
+
+################################################################################
+#
+# Copyright (c) {{cookiecutter.year}} {{cookiecutter.full_name}} All Rights Reserved
+#
+################################################################################
+"""
+This module provide configure file management service in i18n environment.
+
+Authors: {{cookiecutter.full_name}}({{cookiecutter.email}})
+Date:    {{cookiecutter.release_date}}
+"""
+
 """Top-level package for {{ cookiecutter.project_name }}."""
 
 __author__ = """{{ cookiecutter.full_name }}"""
